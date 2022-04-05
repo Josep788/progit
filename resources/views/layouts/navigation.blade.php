9887a96a2881f005073deacccc4b060f0bb715d2
Josep <joseprc24.jrc@gmail.com>
2022-04-05 20:00:44 +0200

    sdfdg

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -59,7 +59,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Cerrar session') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
